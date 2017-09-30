7c1c6705ff147e168804e00581d887d6ff409818
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-09-30 00:25:09 -0400

    added basic Ui

---
+++ harmony-ui/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <string>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_EnterButton_pressed();
+    void on_MessageInput_textChanged();
+
+private:
+    Ui::MainWindow *ui;
+    void post_message();
+};
+
+#endif // MAINWINDOW_H
