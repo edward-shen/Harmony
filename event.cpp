d4c13c85f0a7b40dd48ec53df5a7c625974348fe
Erik Uhlmann <erikuhlmann@gmail.com>
2017-09-29 22:00:52 -0400

    push changes to make build work

---
+++ event.cpp
@@ -0,0 +1,40 @@
+#include "event.h"
+
+namespace harmony {
+    // https://stackoverflow.com/a/16075550
+    template <class T>
+    class SafeQueue {
+    public:
+        SafeQueue(void)
+            : q()
+            , m()
+            , c() {}
+
+        ~SafeQueue(void) {}
+
+        // Add an element to the queue.
+        void enqueue(T t) {
+            std::lock_guard<std::mutex> lock(m);
+            q.push(t);
+            c.notify_one();
+        }
+
+        // Get the "front"-element.
+        // If the queue is empty, wait till a element is avaiable.
+        T dequeue(void) {
+            std::unique_lock<std::mutex> lock(m);
+            while (q.empty()) {
+                // release lock as long as the wait and reaquire it afterwards.
+                c.wait(lock);
+            }
+            T val = q.front();
+            q.pop();
+            return val;
+        }
+
+    private:
+        std::queue<T> q;
+        mutable std::mutex m;
+        std::condition_variable c;
+    };
+}
