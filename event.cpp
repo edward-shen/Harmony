84010d60ffb2cab0d8f202162111f965f93d410d
Erik Uhlmann <erikuhlmann@gmail.com>
2017-09-30 15:59:10 -0400

    fix event queue

--- event.cpp
+++ event.cpp
@@ -60,96 +60,99 @@ namespace harmony {
     }
 
     void event_process() {
-        std::unique_ptr<Event> evt = g_evt_queue.dequeue();
-        
-        switch (evt->type) {
-            case EventType::SEND_PLAINTEXT: {
-                harmony::conv::conv_message* data = (harmony::conv::conv_message*) evt->event_data;
-                harmony::conv::send_plaintext(data->conv, data->message);
-                delete data;
-                break;
-            }
-            case EventType::MAKE_CONV:
-                harmony::conv::make_conv();
-                break;
-            case EventType::JOIN_CONV: {
-                harmony::conv::invite_notification* inv = (harmony::conv::invite_notification*) evt->event_data;
-                harmony::conv::join_conv(*inv);
-                delete inv;
-                break;
-            }
-            case EventType::INIT_CONN:
-                harmony::spread_init();
-                break;
-            case EventType::INIT_NP1SEC: {
-                std::string* data = (std::string*) evt->event_data;
-                harmony::conv::init_comm(*data);
-                delete data;
-                break;
-            }
-            case EventType::SEND_INVITE: {
-                harmony::conv::invite_out* inv = (harmony::conv::invite_out*) evt->event_data;
-                harmony::conv::send_invite(*inv);
-                delete inv;
-                break;
-            }
-            case EventType::CLOSE_CONN:
+        while (!stop_events) {
+            std::unique_ptr<Event> evt = g_evt_queue.dequeue();
+            if (stop_events) return;
 
-                break;
-            case EventType::PROMPT_INVITE: {
-                harmony::conv::invite_notification* inv = (harmony::conv::invite_notification*) evt->event_data;
-                MainWindow::recieve_conversation_invite(inv);
-                delete inv;
-                break;
-            }
-            case EventType::CONV_JOINED: {
-                std::string* conv_name = (std::string*) evt->event_data;
-                temp_notify_conv_joined(*conv_name);
-                delete conv_name;
-                break;
-            }
-            case EventType::RECV_PLAINTEXT: {
-                harmony::conv::conv_message* msg = (harmony::conv::conv_message*) evt->event_data;
-                MainWindow::recieve_plaintext(msg);
-                delete msg;
-                break;
-            }
-            case EventType::NP1SEC_ERROR:
+            switch (evt->type) {
+                case EventType::SEND_PLAINTEXT: {
+                    harmony::conv::conv_message* data = (harmony::conv::conv_message*) evt->event_data;
+                    harmony::conv::send_plaintext(data->conv, data->message);
+                    delete data;
+                    break;
+                }
+                case EventType::MAKE_CONV:
+                    harmony::conv::make_conv();
+                    break;
+                case EventType::JOIN_CONV: {
+                    harmony::conv::invite_notification* inv = (harmony::conv::invite_notification*) evt->event_data;
+                    harmony::conv::join_conv(*inv);
+                    delete inv;
+                    break;
+                }
+                case EventType::INIT_CONN:
+                    harmony::spread_init();
+                    break;
+                case EventType::INIT_NP1SEC: {
+                    std::string* data = (std::string*) evt->event_data;
+                    harmony::conv::init_comm(*data);
+                    delete data;
+                    break;
+                }
+                case EventType::SEND_INVITE: {
+                    harmony::conv::invite_out* inv = (harmony::conv::invite_out*) evt->event_data;
+                    harmony::conv::send_invite(*inv);
+                    delete inv;
+                    break;
+                }
+                case EventType::CLOSE_CONN:
 
-                break;
-            case EventType::SEND_CIPHERTEXT: {
-                std::string* data = (std::string*) evt->event_data;
-                spread_send(*data);
-                delete data;
-                break;
-            }
-            case EventType::UPDATE_USERS: {
-                std::vector<std::string>* data = (std::vector<std::string>*) evt->event_data;
-                MainWindow::recieve_user_list(data);
-                delete data;
-                break;
-            }
-            case EventType::RECV_CIPHERTEXT: {
-                spread_recv* data = (spread_recv*) evt->event_data;
-                harmony::conv::received_message(data->sender, data->data);
-                delete data;
-                break;
-            }
-            case EventType::USER_LEFT: {
-                std::string* data = (std::string*) evt->event_data;
-                harmony::conv::user_left(*data);
-                delete data;
-                break;
-            }
-            case EventType::CONV_LEAVE: {
-                
-                break;
-            }
-            case EventType::SPREAD_ERROR:
+                    break;
+                case EventType::PROMPT_INVITE: {
+                    harmony::conv::invite_notification* inv = (harmony::conv::invite_notification*) evt->event_data;
+                    MainWindow::recieve_conversation_invite(inv);
+                    delete inv;
+                    break;
+                }
+                case EventType::CONV_JOINED: {
+                    std::string* conv_name = (std::string*) evt->event_data;
+                    temp_notify_conv_joined(*conv_name);
+                    delete conv_name;
+                    break;
+                }
+                case EventType::RECV_PLAINTEXT: {
+                    harmony::conv::conv_message* msg = (harmony::conv::conv_message*) evt->event_data;
+                    MainWindow::recieve_plaintext(msg);
+                    delete msg;
+                    break;
+                }
+                case EventType::NP1SEC_ERROR:
 
-                break;
-            default:
-                throw std::runtime_error("you programmed something wrong");
+                    break;
+                case EventType::SEND_CIPHERTEXT: {
+                    std::string* data = (std::string*) evt->event_data;
+                    spread_send(*data);
+                    delete data;
+                    break;
+                }
+                case EventType::UPDATE_USERS: {
+                    std::vector<std::string>* data = (std::vector<std::string>*) evt->event_data;
+                    MainWindow::recieve_user_list(data);
+                    delete data;
+                    break;
+                }
+                case EventType::RECV_CIPHERTEXT: {
+                    spread_recv* data = (spread_recv*) evt->event_data;
+                    harmony::conv::received_message(data->sender, data->data);
+                    delete data;
+                    break;
+                }
+                case EventType::USER_LEFT: {
+                    std::string* data = (std::string*) evt->event_data;
+                    harmony::conv::user_left(*data);
+                    delete data;
+                    break;
+                }
+                case EventType::CONV_LEAVE: {
+
+                    break;
+                }
+                case EventType::SPREAD_ERROR:
+
+                    break;
+                default:
+                    throw std::runtime_error("you programmed something wrong");
+            }
         }
     }
 }
