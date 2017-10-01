1f736c075a7a0c22c35aea1b98f9e0561b65c3aa
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-09-30 20:06:09 -0400

    commented qt_main.cpp

--- harmony-ui/qt_main.cpp
+++ harmony-ui/qt_main.cpp
@@ -7,6 +7,11 @@ MainWindow* g_main_win;
 
 QApplication* a;
 
+/**
+ * @brief qt_init initializes the Qt window
+ * @param argc command line args passed from main.cpp
+ * @param argv command line values passed from main.cpp
+ */
 void qt_init(int argc, char *argv[]) {
     a = new QApplication(argc, argv);
     qRegisterMetaType<QCustomData>();
@@ -14,6 +19,10 @@ void qt_init(int argc, char *argv[]) {
     g_main_win->show();
 }
 
+/**
+ * @brief qt_run starts and shows the Qt windows
+ * @return exit code
+ */
 int qt_run() {
     return a->exec();
 }
